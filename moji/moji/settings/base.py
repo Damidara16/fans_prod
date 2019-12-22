248ab995917294fc17d9479df5602795c9c53b59
simeon <sajala8624@gmail.com>
2019-12-22 17:33:38 -0500

    'patch.1'

--- moji/moji/settings/base.py
+++ moji/moji/settings/base.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'account',
-    #'pages',
+    'pages',
     'content',
     'rest_framework',
     'rest_framework.authtoken'
@@ -176,17 +176,12 @@ REST_FRAMEWORK = {
     ]
 }
 
-"""import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
-DATABASES['default']['CONN_MAX_AGE'] = 500
-
-CORS_REPLACE_HTTPS_REFERER      = True
-HOST_SCHEME                     = "https://"
-SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT             = True
-SESSION_COOKIE_SECURE           = True
-CSRF_COOKIE_SECURE              = True
-SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
-SECURE_HSTS_SECONDS             = 1000000
-SECURE_FRAME_DENY               = True"""
+CORS_REPLACE_HTTPS_REFERER      = False
+HOST_SCHEME                     = "http://"
+SECURE_PROXY_SSL_HEADER         = None
+SECURE_SSL_REDIRECT             = False
+SESSION_COOKIE_SECURE           = False
+CSRF_COOKIE_SECURE              = False
+SECURE_HSTS_SECONDS             = None
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
+SECURE_FRAME_DENY               = False
--- moji/moji/views.py
+++ moji/moji/views.py
@@ -2,4 +2,4 @@ from django.views import View
 from django.shortcuts import render, redirect, Http404
 
 def home(request):
-    return render(request, 'home/home.html')
+    return render(request, 'pages/moji/home.html')
